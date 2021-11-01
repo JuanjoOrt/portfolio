489265705d09fcef71074e83263b944089cc4409
Juanjo Ortiz <juanjoseortizgames@gmail.com>
2021-11-01 21:33:40 +0100

    fix margin and text

--- components/pages/Home.js
+++ components/pages/Home.js
@@ -2,11 +2,13 @@
 export default function Home () {
   return <section>
       <div className='container'>
-        <div className='home-subtitle'>HI, I’m</div>
-        <div className='home-title'>Juanjo Ortiz</div>
-        <div className='home-description'>Soy desarrollador Front-End con más de 3 años de experiencia.
-         Actualmente desarrollo aplicaciones enfocadas a facilitar el aprendizaje
-         de los alumnos en <a href='https://www.ilerna.es' className='link' target='_blank' rel='noreferrer'>Ilerna</a></div>
+        <div className='home-container'>
+          <div className='home-subtitle'>HI, I’m</div>
+          <div className='home-title'>Juanjo Ortiz</div>
+          <div className='home-description'>Soy desarrollador Front-End con más de 3 años de experiencia. <br />
+          Actualmente desarrollo aplicaciones enfocadas a facilitar el aprendizaje
+          de los alumnos en <a href='https://www.ilerna.es' className='link' target='_blank' rel='noreferrer'>Ilerna</a></div>
+        </div>
       </div>
   </section>
 }
