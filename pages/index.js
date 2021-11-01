8d48635af45b41d2578c3ad44cd1fd630fe45811
Juanjo Ortiz <juanjoseortizgames@gmail.com>
2021-11-01 18:41:02 +0100

    head added

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,13 @@
 
 export default function Home () {
   return (
-    <div> Juanjos portfolio </div>
+    <div>
+      <head>
+        <title>Juanjo Ortiz</title>
+      </head>
+      <body>
+        Juanjo Ortiz
+      </body>
+    </div>
   )
 }
